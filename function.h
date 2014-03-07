17329e3d2111006ff3e7f9cdc1534c1f84628516
Sedovlas96 <Egolior@mail.ru>
2014-03-07 15:00:19 +0300

    prototypes

---
+++ function.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+void mergeParts( int *Array, int firstIndexOfArrayOne, int lastIndexOfArrayOne, int firstIndexOfArrayTwo, int lastIndexOfArrayTwo );
+void mergeSorting( int *Array, int firstIndex, int lastIndex );
+void generateArray( int *Array, int length, int value );
+void showArray( int *Array, int length );
+
+
+#endif // FUNCTION_H
